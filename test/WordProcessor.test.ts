be800b83a885c2d3115ebe00bb315c0a6dca8f26
Ori Gold <nehoraigold@gmail.com>
2022-03-26 19:53:16 +0300

    testing framework added

---
+++ test/WordProcessor.test.ts
@@ -0,0 +1,11 @@
+import { describe, it } from 'mocha';
+import { expect } from 'chai';
+import { WordProcessor } from '../src/word_processor/WordProcessor';
+
+describe(WordProcessor.prototype.constructor.name, () => {
+    describe(WordProcessor.prototype.Process.name, () => {
+        it('should ...', () => {
+            expect(true).to.be.true;
+        });
+    });
+});
